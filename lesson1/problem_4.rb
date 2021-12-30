91c463ae673de1fec4217a74bcaefacb15c388ee
Aliaksei Hovar <73116496+Spola1@users.noreply.github.com>
2021-12-30 13:30:01 +0300

    each do

--- lesson1/problem_4.rb
+++ lesson1/problem_4.rb
@@ -4,8 +4,8 @@ from the product of two 2-digit numbers is 9009 = 91 × 99.
 Find the largest palindrome made from the product of two 3-digit numbers.
 =end
 palindromes=[]
-for i in 100..999 do
-  for j in 100..999 do
+(100..999).each do |i|
+  (100..999).each do |j|
     k=i*j
     palindromes << k if k.to_s==k.to_s.reverse
   end
